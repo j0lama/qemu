d22a20726b4ca9817da46955bfe720e373c90625
Jon <jon_larrea@hotmail.com>
2018-11-14 23:22:26 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,5 +3,9 @@ echo "Installing dependencies for compile QEMU in Ubuntu 14"
 sudo apt install gcc g++ libtool pkg-config autoconf libglib2.0-dev make
 git submodule update --init pixman
 git submodule update --init dtc
+./configure
+make
+chmod +x run.sh debug.sh
 mv run.sh ../run.sh
 mv debug.sh ../debug.sh
+cd ..
